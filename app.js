8ab290751f370653e8d03926e8f118b0c7761a73
Jashnoor Singh <jashnoorsingh2005@gmail.com>
2026-08-14 03:31:38 +0530

    final stages

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 const urlRoute = require('./routes/url');
 const express = require('express');
+const URL = require('./models/url');
 const app = express();
 const connectToMongoDB = require('./connect');
 connectToMongoDB(app);
 
 app.use(express.json());
 app.use('/url', urlRoute);
+
+app.get('/:shortID', async (req, res)=>{
+    const shortID = req.params.shortID;
+    const entry = await URL.findOneAndUpdate({
+        shortID
+    }, {$push: {
+        visitHistory : {
+            timestamp : Date.now()
+        }
+    }});
+
+    res.redirect(entry.redirectURL);
+});
